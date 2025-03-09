ca7139df63ca1875b486b4e8eacf874fa4435705
yu1ia <vasilenyuky@gmail.com>
2025-03-09 22:05:19 +0200

    client skeleton

--- src/components/timestamp-fragment.tsx
+++ src/components/timestamp-fragment.tsx
@@ -1,9 +1,15 @@
 "use client"
 
 import dynamic from "next/dynamic";
+import { Skeleton } from "./ui/skeleton";
 
 
-const TimeRow = dynamic(() => import('./time-row').then(comp => comp.TimeRow), { ssr: false })
+const TimeRow = dynamic(() => import('./time-row').then(comp => comp.TimeRow),
+    {
+        loading: () => <Skeleton className="w-[150px] h-5 mb-1" />,
+        ssr: false
+    }
+)
 
 export const TimestampFragment = ({ item }: { item: string }) => (
     <><TimeRow item={item} /></>
